1c6c47a1c6a831f6165bb8d40395b2700982efe8
JBC <josmarbcristello@gmail.com>
2023-06-07 20:19:14 -0600

    Centralized plotting function, more efficient on memmory

--- data processing/data_utils.py
+++ data processing/data_utils.py
@@ -1,7 +1,7 @@
 import os
 import tempfile
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 def import_lvm_file(file_path, chunksize=100000):
     # Find the header row number
@@ -44,3 +44,40 @@ def import_lvm_file(file_path, chunksize=100000):
     
     data = pd.concat(chunks, ignore_index=True)
     return data
+
+def plot_data(data_frame, column_names=["CH5"], start_time=None, end_time=None, transparency=1,
+              title=None, xlabel='Time [s]', ylabel='Value [V]'):
+    # If no column names provided, default to an empty list.
+    if not column_names:
+        column_names = []
+
+    # If only a single column name is provided, wrap it in a list.
+    if isinstance(column_names, str):
+        column_names = [column_names]
+
+    # Default to using all data if start_time or end_time isn't specified.
+    if start_time is None:
+        start_time = data_frame['time'].min()
+    if end_time is None:
+        end_time = data_frame['time'].max()
+
+    # Filter data_frame based on the provided start and end times.
+    mask = (data_frame['time'] >= start_time) & (data_frame['time'] <= end_time)
+    filtered_data = data_frame.loc[mask, column_names]
+
+    # Plot the columns with time_values as the x-axis
+    for column in filtered_data.columns:
+        plt.plot(data_frame.loc[mask, 'time'], filtered_data[column], label=column, alpha=transparency)
+
+    # Customize the plot
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    if title:
+        plt.title(title)
+    plt.legend(filtered_data.columns, loc='best')
+
+    # Set x-axis limits
+    plt.xlim(start_time, end_time)
+
+    # Show the plot
+    plt.show()
